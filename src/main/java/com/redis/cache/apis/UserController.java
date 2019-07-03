18fc2b5f3a3753e44a8f04fb80530ff889bcbd89
ahmed <eessaouira@gmail.com>
2019-07-03 21:55:50 +0100

    first commit

---
+++ src/main/java/com/redis/cache/apis/UserController.java
@@ -0,0 +1,47 @@
+package com.redis.cache.apis;
+
+import com.redis.cache.models.User;
+import com.redis.cache.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Cacheable(value = "users", key = "#userId")
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    public Optional<User> getUser(@PathVariable String userId) {
+        LOG.info("Getting user with ID {}.", userId);
+        return userRepository.findById(Long.valueOf(userId));
+    }
+
+    @CachePut(value = "users", key = "#user.id")
+    @PutMapping("/update")
+    public User updatePersonByID(@RequestBody User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    @CacheEvict(value = "users", allEntries=true)
+    @DeleteMapping("/{id}")
+    public void deleteUserByID(@PathVariable Long id) {
+        LOG.info("deleting person with id {}", id);
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/redis/cache/models/User.java
@@ -0,0 +1,62 @@
+package com.redis.cache.models;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 7156526077883281623L;
+
+    @Id
+    @SequenceGenerator(name = "SEQ_GEN", sequenceName = "SEQ_USER", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
+    private Long id;
+    private String name;
+    private long followers;
+
+    public User() {
+    }
+
+    public User(String name, long followers) {
+        this.name = name;
+        this.followers = followers;
+    }
+
+    //standard getters and setters
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(long followers) {
+        this.followers = followers;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User{id=%d, name='%s', followers=%d}", id, name, followers);
+    }
+}
